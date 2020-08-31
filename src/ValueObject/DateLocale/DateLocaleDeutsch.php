2d7759314400e5345a3240c3b078362c328417ed
Th. Abplanalp <info@tasoft.ch>
2020-08-31 18:31:59 +0200

    ADD: locales

---
+++ src/ValueObject/DateLocale/DateLocaleDeutsch.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace TASoft\Util\ValueObject\DateLocale;
+
+
+class DateLocaleDeutsch extends DateLocale
+{
+	public $longDayNames = [
+		"Sonntag",
+		"Montag",
+		"Dienstag",
+		"Mittwoch",
+		"Donnerstag",
+		"Freitag",
+		"Samstag"
+	];
+
+	public $shortDayNames = [
+		"So",
+		"Mo",
+		"Di",
+		"Mi",
+		"Do",
+		"Fr",
+		"Sa"
+	];
+
+	public $longMonthNames = [
+		"Januar",
+		"Februar",
+		"März",
+		"April",
+		"Mai",
+		"Juni",
+		"Juli",
+		"August",
+		"September",
+		"Oktober",
+		"November",
+		"Dezember"
+	];
+
+	public $shortMonthNames = [
+		"Jan",
+		"Feb",
+		"Mär",
+		"Apr",
+		"Mai",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Okt",
+		"Nov",
+		"Dez"
+	];
+}
---
+++ src/ValueObject/DateLocale/DateLocalePortugese.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace TASoft\Util\ValueObject\DateLocale;
+
+
+class DateLocalePortugese extends DateLocale
+{
+	public $longDayNames = [
+		"Domingo",
+		"Segunda-Feira",
+		"Terça-Feira",
+		"Quarta-Feira",
+		"Quinta-Feira",
+		"Sexta-Feira",
+		"Sábado"
+	];
+
+	public $shortDayNames = [
+		"Dom",
+		"Seg",
+		"Ter",
+		"Qua",
+		"Qui",
+		"Sex",
+		"Sáb"
+	];
+
+	public $longMonthNames = [
+		"Janeiro",
+		"Fevereiro",
+		"Março",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Setembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro"
+	];
+
+	public $shortMonthNames = [
+		"Jan",
+		"Fev",
+		"Mar",
+		"Abr",
+		"Mai",
+		"Jun",
+		"Jul",
+		"Ago",
+		"Set",
+		"Out",
+		"Nov",
+		"Dez"
+	];
+}
